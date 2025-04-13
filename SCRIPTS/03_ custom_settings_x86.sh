8ec9c2e967b49c09209522ce9a2a35f34ef1ea2c
OPPEN321 <zj18139624826@gmail.com>
2025-04-13 23:53:12 +0800

    Update and rename 03_ custom_settings.sh to 03_ custom_settings_x86.sh

---
+++ SCRIPTS/03_ custom_settings_x86.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+mkdir -p files/usr/bin
+
+AGH_CORE=$(curl -sL https://api.github.com/repos/AdguardTeam/AdGuardHome/releases/latest | grep /AdGuardHome_linux_amd64 | awk -F '"' '{print $4}')
+
+wget -qO- $AGH_CORE | tar xOvz > files/usr/bin/AdGuardHome
+
+chmod +x files/usr/bin/AdGuardHome
+
+mkdir -p files/etc/openclash/core
+
+CLASH_META_URL="https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-amd64.tar.gz"
+GEOIP_URL="https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat"
+GEOSITE_URL="https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat"
+
+wget -qO- $CLASH_META_URL | tar xOvz > files/etc/openclash/core/clash_meta
+wget -qO- $GEOIP_URL > files/etc/openclash/GeoIP.dat
+wget -qO- $GEOSITE_URL > files/etc/openclash/GeoSite.dat
+
+chmod +x files/etc/openclash/core/clash*
